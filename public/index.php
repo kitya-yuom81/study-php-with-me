35790e479fe0e7e9d6d32384fe1535369630792e
Yuom Kitya <kityayuom@gmail.com>
2025-09-16 22:30:18 +0700

    update

--- public/index.php
+++ public/index.php
@@ -1,11 +1,10 @@
 <?php
 declare(strict_types=1);
-
 session_start();
 
 require __DIR__ . '/../app/config.php';
 require __DIR__ . '/../app/Router.php';
-require __DIR__ . '/../app/TaskModel.php';
+require __DIR__ . '/../app/TaskModelSqlite.php';
 require __DIR__ . '/../app/TaskController.php';
 
 use App\Router;
@@ -13,7 +12,6 @@ use App\TaskController;
 
 $router = new Router();
 
-// routes
 $router->get('/', [TaskController::class, 'index']);
 $router->post('/task/create', [TaskController::class, 'create']);
 $router->post('/task/toggle', [TaskController::class, 'toggle']);
@@ -21,5 +19,4 @@ $router->post('/task/delete', [TaskController::class, 'delete']);
 $router->post('/task/update', [TaskController::class, 'update']);
 $router->post('/task/clear-completed', [TaskController::class, 'clearCompleted']);
 
-// dispatch
 $router->dispatch();
--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,6 @@
   <meta charset="utf-8">
   <title>PHP Todo</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- ASSETS is a GLOBAL constant defined in config.php -->
   <link href="<?= ASSETS ?>/style.css" rel="stylesheet">
 </head>
 <body>
